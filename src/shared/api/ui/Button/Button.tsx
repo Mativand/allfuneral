4a3e2b0a944549028a1579c2107556b527c5a011
Matfei <64943619+Mativand@users.noreply.github.com>
2025-04-16 12:16:06 -0500

    Update Button component

--- src/shared/api/ui/Button/Button.tsx
+++ src/shared/api/ui/Button/Button.tsx
@@ -5,22 +5,24 @@ import styles from './Button.module.scss';
 type Props = {
   variant?: 'filled' | 'outlined';
   iconUrl?: string;
-  text: string;
+  text?: string;
   onClick?: () => void;
+  icon?: string;
 };
 
-const Button: React.FC<Props> = ({ variant, text, onClick }) => {
+const Button: React.FC<Props> = ({ variant, text, onClick, icon }) => {
   const buttonClass = [
     styles.button,
-    variant && styles[`button--${variant}`]
+    variant && styles[`button--${variant}`],
+    !text && styles.button__noText
   ]
     .filter(Boolean)
     .join(' ');
 
   return (
     <button className={buttonClass} onClick={onClick}>
-      <span className={`${styles.button__icon} _icon-add`}></span>
-      <span className={styles.button__text}>{text}</span>
+      <span className={`${styles.button__icon} _icon-${icon}`}></span>
+      {text && <span className={styles.button__text}>{text}</span>}
     </button>
   );
 };
