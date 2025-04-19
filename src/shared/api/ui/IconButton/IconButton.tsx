d30cd0813c34c037e970a079de52f3d6bb24b2d5
Matfei <64943619+Mativand@users.noreply.github.com>
2025-04-19 11:38:21 -0500

    Enhance IconButton component to accept an optional color prop for icon styling

--- src/shared/api/ui/IconButton/IconButton.tsx
+++ src/shared/api/ui/IconButton/IconButton.tsx
@@ -3,12 +3,16 @@ import styles from "./IconButton.module.scss";
 interface IconButtonProps {
   icon: string;
   onClick: () => void;
+  color?: string;
 }
 
-const IconButton = ({ icon, onClick }: IconButtonProps) => {
+const IconButton = ({ icon, onClick, color }: IconButtonProps) => {
   return (
     <button className={styles.iconButton} onClick={onClick}>
-      <span className={`${styles.iconButton__icon} _icon-${icon}`}></span>
+      <span 
+        className={`${styles.iconButton__icon} _icon-${icon}`}
+        style={{ color }}
+      ></span>
     </button>
   );
 };
