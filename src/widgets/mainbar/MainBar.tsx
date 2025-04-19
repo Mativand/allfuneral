e5f5dfd16a8015b27f3d212cf1ac9bf9b73b5d8c
Matfei <64943619+Mativand@users.noreply.github.com>
2025-04-18 20:22:16 -0500

    Add MainBar widget

---
+++ src/widgets/mainbar/MainBar.tsx
@@ -0,0 +1,46 @@
+import styles from "./MainBar.module.scss";
+
+const MainBar = () => {
+  return (
+    <div className={styles.mainBar}>
+      <nav className={styles.mainBar__nav}>
+        <div className={styles.mainBar__navTop}>
+          <ul className={styles.mainBar__navList}>
+            <li className={styles.mainBar__navTopItem}>
+              <a href="#" className={styles.logo}>
+                <span className={"_icon-logo"}></span>
+              </a>
+            </li>
+            <li className={styles.mainBar__navTopItem}>
+              <a href="#" className={styles.mainBar__navLink}>
+                <span className={"_icon-company"}></span>
+              </a>
+            </li>
+            <li className={styles.mainBar__navTopItem}>
+              <a href="#" className={styles.mainBar__navLink}>
+                <span className={"_icon-contractor"}></span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.mainBar__line}></div>
+        <div className={styles.mainBar__navBottom}>
+          <ul className={styles.mainBar__navList}>
+            <li className={styles.mainBar__navBottomItem}>
+              <a href="#" className={styles.mainBar__navLink}>
+                <span className={"_icon-settings"}></span>
+              </a>
+            </li>
+            <li className={styles.mainBar__navBottomItem}>
+              <a href="#" className={styles.mainBar__navLink}>
+                <span className={"_icon-signOut"}></span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default MainBar;
