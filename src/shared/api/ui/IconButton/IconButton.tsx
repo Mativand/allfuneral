3410fb2fe31fae5772dea6b12a02ed274d081fdd
Matfei <64943619+Mativand@users.noreply.github.com>
2025-04-19 11:28:18 -0500

    Update IconButton component

--- src/shared/api/ui/IconButton/IconButton.tsx
+++ src/shared/api/ui/IconButton/IconButton.tsx
@@ -1,14 +1,16 @@
-import styles from './IconButton.module.scss';
+import styles from "./IconButton.module.scss";
 
 interface IconButtonProps {
-    icon: string;
-    onClick: () => void;
+  icon: string;
+  onClick: () => void;
 }
 
 const IconButton = ({ icon, onClick }: IconButtonProps) => {
-    return <button className={styles.iconButton} onClick={onClick}>
-      <span className={`${styles.button__icon} _icon-${icon}`}></span>
-      </button>
-}
+  return (
+    <button className={styles.iconButton} onClick={onClick}>
+      <span className={`${styles.iconButton__icon} _icon-${icon}`}></span>
+    </button>
+  );
+};
 
 export default IconButton;
