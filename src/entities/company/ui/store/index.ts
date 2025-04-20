def5fec82de21e01f187d43866c1c214f855ef4c
Matfei <64943619+Mativand@users.noreply.github.com>
2025-04-20 09:58:56 -0500

    Add CompanyStore for managing company state with MobX

---
+++ src/entities/company/ui/store/index.ts
@@ -0,0 +1,24 @@
+import { makeAutoObservable } from "mobx";
+import { ICompany } from "../../types";
+
+export class CompanyStore {
+  #company: ICompany | null = null;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  getCompany = () => {
+    return this.#company;
+  }
+
+  setCompany = (company: ICompany) => {
+    this.#company = company;
+  }
+
+  updateCompany = (company: ICompany) => {
+    this.#company = company;
+  }
+}
+
+export const companyStore = new CompanyStore();
